5c25f72790dd268081a6f2a2448d2ee22a8ed7ef
Guillaume Tucker <guillaume.tucker@collabora.com>
2023-01-31 10:54:34 +0100

    bisect.jpl: use kci_build --storage-config
    Use the new kci_build --storage-config / --storage-cred arguments with
    the KCI_STORAGE_CONFIG job parameter.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- jobs.groovy
+++ jobs.groovy
@@ -339,6 +339,7 @@ pipelineJob('lava-bisection') {
     stringParam('KCI_API_URL', KCI_API_URL, 'URL of the KernelCI back-end API.')
     stringParam('KCI_API_TOKEN_ID', KCI_API_TOKEN_ID, 'Identifier of the KernelCI backend API token stored in Jenkins.')
     stringParam('KCI_STORAGE_URL', KCI_STORAGE_URL, 'URL of the KernelCI storage server.')
+    stringParam('KCI_STORAGE_CONFIG', KCI_STORAGE_CONFIG, 'Name of the KernelCI storage configuration.')
     stringParam('KCI_DB_CONFIG', KCI_DB_CONFIG, 'Value to use with the --db-config argument')
     stringParam('KCI_CORE_URL', KCI_CORE_URL, 'URL of the kernelci-core repository.')
     stringParam('KCI_CORE_BRANCH', KCI_CORE_BRANCH, 'Name of the branch to use in the kernelci-core repository.')
